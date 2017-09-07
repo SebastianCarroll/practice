c4301b9ae302656fe15ffe8565b4b97af8893f31
Sebastian Carroll <scarroll@hortonworks.com>
2017-09-07 19:26:14 +0100

    Added method to map the repeated chars to an array of string

--- longest_substring/longest_repeated_str.java
+++ longest_substring/longest_repeated_str.java
@@ -11,6 +11,26 @@ class Solution {
     }
 
     private static String longest_repeating(String s) {
-        return s;
+        // Split string to String[] where each are repeating
+        String[] repeating = map_to_repeated(s);
+        // Filter all but longest
+        // return that one
+        return repeating[0];
+    }
+
+    private static String[] map_to_repeated(String s) {
+        // Here I'm going to need to know how many elements to make 
+        // Return value. I could just use an arrayList or something similar
+        // But seems wasteful.
+        // Other wise i'll need to scan the list to count the transitions. 
+        // Could just use same length as string. Won't be more than this
+
+        String[] splits = new String[s.length()];
+        String[] chars = s.split("");
+        for(int i=0; i<s.length(); i++){
+            splits[i] = chars[i];
+        }
+
+        return splits;
     }
 }
