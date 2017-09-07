82219015d95cba6b2857fa521af00163ee19b24c
Sebastian Carroll <scarroll@hortonworks.com>
2017-09-07 18:59:37 +0100

    Added prompts and required method

--- longest_substring/longest_repeated_str.java
+++ longest_substring/longest_repeated_str.java
@@ -3,7 +3,14 @@ import java.util.*;
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer i = sc.nextInt();
-        System.out.printf("You entered: %d%n", i);
+        System.out.println("Enter the string to test");
+        String i = sc.next();
+        System.out.printf("You entered: %s%n", i);
+        System.out.println("Longest repeating character substring is: " + 
+                longest_repeating(i));
+    }
+
+    private static String longest_repeating(String s) {
+        return s;
     }
 }
